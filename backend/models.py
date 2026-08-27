ffec29ec3716f911dfc0cc9cab918641e94078ca
keerthanaworld <keerthana.2153025@srec.ac.in>
2026-08-27 23:00:40 +0530

    fix: correct model import

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from datetime import datetime,timezone
 from sqlalchemy import Column, Integer, String, DateTime , ForeignKey
 from sqlalchemy.orm import relationship
 
-from .database import Base
+from database import Base
 
 class Url(Base):
     __tablename__ = "urls"
