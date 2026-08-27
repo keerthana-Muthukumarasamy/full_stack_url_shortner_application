fdff6f5e71870aea9df812a018d980b203e990e1
keerthanaworld <keerthana.2153025@srec.ac.in>
2026-08-27 23:01:22 +0530

    feat: initialize database tables

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-app=FastAPI()
+
+from database import Base, engine
+import models
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
 
 @app.get("/")
 def root():
-    return {"message": "url shortner api is working"}
+    return {"message": "URL Shortener API is running"}
