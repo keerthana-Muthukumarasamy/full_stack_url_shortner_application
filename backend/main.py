fe39e8cc7e05a780ee8fc2da70e244a9006c29a7
keerthanaworld <keerthana.2153025@srec.ac.in>
2026-08-28 15:10:41 +0530

    feat: add redirect and URL listing

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,16 @@ import models
 from schemas import URLCreate
 import secrets
 import string
+from fastapi import HTTPException
+from fastapi.responses import RedirectResponse
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def generate_short_code(length: int =6)-> str:
+    characters = string.ascii_letters + string.digits
+    return "".join(secrets.choice(characters) for _ in range(length))
 
 
 @app.get("/")
@@ -38,6 +44,47 @@ def create_url(
         "short_code": new_url.short_code,
         "short_url": f"http://localhost:8000/{new_url.short_code}",
     }
-def generate_short_code(length: int =6)-> str:
-    characters = string.ascii_letters + string.digits
-    return "".join(secrets.choice(characters) for _ in range(length))
+
+@app.get("/api/urls")
+def get_urls(
+    db: Session = Depends(get_db)
+):
+    urls = (
+        db.query(models.Url)
+        .order_by(models.Url.created_at.desc())
+        .all()
+    )
+
+    return urls
+
+
+@app.get("/{short_code}")
+def redirect_url(
+    short_code: str,
+    db: Session = Depends(get_db)
+):
+    url = db.query(models.Url).filter(
+        models.Url.short_code == short_code
+    ).first()
+
+    if not url:
+        raise HTTPException(
+            status_code=404,
+            detail="Short URL not found"
+        )
+
+    url.click_count += 1
+
+    click_event = models.ClickEvent(
+        url_id=url.id
+    )
+
+    db.add(click_event)
+    db.commit()
+
+    return RedirectResponse(
+        url=url.original_url,
+        status_code=307
+    )
+
+
